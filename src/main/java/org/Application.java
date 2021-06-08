93566c63a104c485594dbf21a67ecc2d0f2a2931
d310n <deiongarcia@gmail.com>
2021-06-08 11:02:14 -0500

    Added properties and changelog

--- src/main/java/org/Application.java
+++ src/main/java/org/Application.java
@@ -1,4 +1,4 @@
-package org.application;
+package org;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +9,6 @@ public class Application
     public static void main(String[] args)
     {
         SpringApplication.run(Application.class, args);
-        System.out.println("Score keeper app works!");
+        System.out.println("Score keeper works!");
     }
 }
