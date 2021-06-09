9764d9db720065b7304cb2b5e824ecda65a2660d
d310n <deiongarcia@gmail.com>
2021-06-09 16:02:20 -0500

    Added logger

--- src/main/java/com/scorekeeper/Scorekeeper.java
+++ src/main/java/com/scorekeeper/Scorekeeper.java
@@ -1,14 +1,18 @@
 package com.scorekeeper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Scorekeeper
 {
+    private static final Logger logger = LoggerFactory.getLogger(Scorekeeper.class);
+
     public static void main(String[] args)
     {
         SpringApplication.run(Scorekeeper.class, args);
-        System.out.println("Score keeper works!");
+        logger.info("Score keeper works!");
     }
 }
