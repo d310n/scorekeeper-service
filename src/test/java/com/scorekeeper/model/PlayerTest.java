03ff09b4121d69ac4263ab17369786ac44a293b9
d310n <deiongarcia@gmail.com>
2021-06-14 11:43:57 -0500

    Added unit testing for player and team

---
+++ src/test/java/com/scorekeeper/model/PlayerTest.java
@@ -0,0 +1,34 @@
+package com.scorekeeper.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @org.junit.jupiter.api.Test
+    void getTeams() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setTeams() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getId() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFirstName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setFirstName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLastName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setLastName() {
+    }
+}
---
+++ src/test/java/com/scorekeeper/model/TeamTest.java
@@ -0,0 +1,28 @@
+package com.scorekeeper.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TeamTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getTeamName() {
+    }
+
+    @Test
+    void setTeamName() {
+    }
+
+    @Test
+    void getTeamState() {
+    }
+
+    @Test
+    void setTeamState() {
+    }
+}
